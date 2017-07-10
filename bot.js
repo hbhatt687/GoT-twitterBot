7d1e99adfd335f3350896919f9e5339e08cdef9d
Harsh Bhatt <hbhatt687@gmail.com>
2017-07-09 23:26:38 -0400

    posts hello world

--- bot.js
+++ bot.js
@@ -4,3 +4,20 @@ var Twit = require('twit');
 var config = new require('./config');
 
 var T = new Twit(config);
+
+var params = {
+	status: 'hello world!'
+}
+
+//
+//  tweet 'hello world!'
+//
+T.post('statuses/update', params, getData);
+
+function getData(err, data, response) {
+	if (err) {
+		console.log("Something went wrong!");
+	} else {
+		console.log("It worked!");
+	}
+}
