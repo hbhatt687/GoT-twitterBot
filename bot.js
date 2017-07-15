2d001ef7cf2971fcdc55962a493ba1faaaa5670f
Harsh Bhatt <hbhatt687@gmail.com>
2017-07-15 15:24:55 -0400

    readFile() successfully retirieves updates quotes

--- bot.js
+++ bot.js
@@ -14,8 +14,8 @@ var config = new require('./config');
 var T = new Twit(config);
 var fs = require('fs');
 var request = require("request");
-var	cheerio = require("cheerio");
-var	url = "https://en.wikiquote.org/wiki/A_Song_of_Ice_and_Fire";
+var cheerio = require("cheerio");
+var url = "https://en.wikiquote.org/wiki/A_Song_of_Ice_and_Fire";
 var quoteIndex = 8;
 
 
